dc4a9d17667342332ea267bd66e066520599a5da
Jayanth Chelur Kriyaraju <jayanth.kriyaraju@kreuzwerker.de>
2020-04-07 10:40:45 +0200

    Removed global folder

--- global/state/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "dynamodb_table" {
-  description = "The DynamoDB lock table"
-  value       = "${module.state.dynamodb_table}"
-}
-
-output "id" {
-  description = "The S3 state bucket"
-  value       = "${module.state.id}"
-}
